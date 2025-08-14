65a6c1ccf8541da343ba8750624d4f9110a3b70d
Mariela Tihova <MTihova@infragistics.com>
2025-08-14 11:04:09 +0300

    Removing old folder structure

--- projects/erp-hgrid/src/app/app.config.ts
+++
@@ -1,7 +0,0 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig } from '@angular/core';
-import { provideClientHydration } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideClientHydration(), provideAnimationsAsync(), provideHttpClient(withFetch())] };
